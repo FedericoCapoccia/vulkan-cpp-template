8babec18309957eb629bc6436d642c08cbc71c4f
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2025-03-23 00:32:14 +0100

    SOAP is a piece of shit

--- src/application.cpp
+++ src/application.cpp
@@ -1,9 +1,6 @@
 #include "logging/logger.hpp"
 #include <vulkan/vulkan.hpp>
 
-const uint32_t WIDTH = 800;
-const uint32_t HEIGHT = 600;
-
 auto main() -> int
 {
     vkrend::Logger::init();
--- src/pch.hpp
+++ src/pch.hpp
@@ -1,3 +1,12 @@
 #pragma once
 
 #include "logging/logger.hpp"
+#include <deque>
+#include <expected>
+#include <functional>
+#include <memory>
+#include <optional>
+#include <ranges>
+#include <set>
+#include <string>
+#include <vector>
